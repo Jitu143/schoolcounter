c9cecd9f75321a695bf1a4a00e4993e302e57a80
Jitu143 <jitendrajain92@gmail.com>
2016-05-22 08:16:14 +0530

    Create home page blocks

--- docroot/sites/all/themes/sub/schoolcounter_theme/javascripts/homepage.js
+++ docroot/sites/all/themes/sub/schoolcounter_theme/javascripts/homepage.js
@@ -8,6 +8,49 @@
         slidesToShow: 1,
         adaptiveHeight: true
       });
+
+      $('#block-views-views-home-page-block .view-content').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 800,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+      });
+
+      $('#block-views-views-home-page-block-1 .view-content').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
     }
   };
 })(jQuery);
